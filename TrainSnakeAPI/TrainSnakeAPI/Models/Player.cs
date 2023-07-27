8de5ee8b0e5e367d43e008dba7933adfcdcdbb2c
= <ryanneilparker@gmail.com>
2023-07-27 21:31:30 +0200

    Setup typescript environment

--- TrainSnakeAPI/TrainSnakeAPI/Models/Player.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrainSnakeAPI.Models
-{
-	public class Player
-	{
-		public int Id { get; set; }
-		public string? UserName { get; set; }
-		public DateTime CreatedDate {  get; set; }
-	}
-}
--- TrainSnakeAPI/TrainSnakeAPI/Models/ScoreResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TrainSnakeAPI.Models
-{
-	public class ScoreResult
-	{
-		public string playerName { get; set; }
-		public int playerScore { get; set; }
-	}
-}
